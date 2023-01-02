90b0a34fce9be48bc57d77eb379f170fce448695
tocom <tcom242242@gmail.com>
2023-01-02 15:09:46 +0900

    add basic model

---
+++ search_similar_image/model/search_model.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+from typing import List, Tuple
+
+import faiss
+import numpy as np
+import timm
+import torch
+from PIL import Image
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class SearchModel:
+    def __init__(self, model_name: str = "vit_base_patch16_224"):
+        self.model = timm.create_model(model_name, pretrained=True, num_classes=0)
+        self.model.to(device)
+        self.model.eval()
+        config = resolve_data_config({}, model=self.model)
+        self.transform = create_transform(**config)
+        self.index = None
+        self.source_img_paths = None
+
+    def fit(self, source_dir: str):
+        self.source_img_paths = np.array([str(p) for p in Path(source_dir).glob("*")])
+        features = []
+        for source_img_path in self.source_img_paths:
+            img = Image.open(source_img_path).convert("RGB")
+            feature = self.extract_feature(img)
+            features.append(feature.squeeze(0).numpy())
+        features = np.array(features)
+
+        self.index = faiss.IndexFlatL2(features.shape[1])
+        self.index.add(features)
+
+    def extract_feature(self, img: Image) -> torch.Tensor:
+        with torch.no_grad():
+            img_tensor = self.transform(img)
+            feature = self.model(img_tensor.unsqueeze(0))
+        return feature
+
+    def predict(self, target_img_path: str) -> Tuple[List, List]:
+        img = Image.open(target_img_path).convert("RGB")
+        feature = self.extract_feature(img)
+        feature = feature.numpy()
+
+        D, indexes = self.index.search(feature, 5)
+        neighbor_img_paths = self.source_img_paths[indexes]
+        return D, neighbor_img_paths
+
+
+if __name__ == "__main__":
+    model = SearchModel()
--- tests/test_search_img.py
+++ tests/test_search_img.py
@@ -1,5 +1,49 @@
+import numpy as np
+import pytest
+from PIL import Image
+
 from search_similar_image import __version__
+from search_similar_image.model.search_model import SearchModel
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@pytest.fixture
+def image_file(tmp_path_factory):
+    img = Image.new("RGB", (320, 320))
+    fn = tmp_path_factory.mktemp("data") / "img.png"
+    img.save(fn)
+    yield fn
+
+
+@pytest.fixture
+def image_dir(tmp_path_factory):
+    img = Image.new("RGB", (320, 320))
+    image_dir_path = tmp_path_factory.mktemp("data")
+    fn1 = image_dir_path / "img.png"
+    img.save(fn1)
+    img = Image.new("RGB", (320, 320))
+    fn2 = image_dir_path / "img2.png"
+    img.save(fn2)
+    yield image_dir_path
+
+
+class TestSearchModel:
+    def test_init_serch_model(self):
+        model = SearchModel()
+        assert model is not None
+
+    def test_fit(self, image_dir):
+        model = SearchModel()
+        model.fit(image_dir)
+        assert model.index is not None
+        assert type(model.source_img_paths) is np.ndarray
+
+    def test_predict(self, image_dir, image_file):
+        model = SearchModel()
+        model.fit(image_dir)
+        scores, paths = model.predict(image_file)
+        assert type(paths) is np.ndarray
+        assert type(scores) is np.ndarray
